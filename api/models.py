e1793d2cc5d3c47da51dfb85f121aeefd4f680d2
howsun <howsun7@gmail.com>
2022-06-07 17:16:25 -0700

    add Curriculum and Topic

--- api/models.py
+++ api/models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
     password = db.Column(db.String(128))
+    curriculums = db.relationship('Curriculum', backref='creator', lazy='dynamic')
 
     def hash_password(self, password):
         self.password = ca_context.encrypt(password)
@@ -21,12 +22,20 @@ class User(db.Model):
         return ca_context.verify(password, self.password)
 
 
-# class Curriculums(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80))
-#     creater_id = db.Column(db.Integer, db.ForeignKey(""))
+class Curriculum(db.Model):
 
-# class Topics(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80))
+    __tablename__ = 'curriculums'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    creater_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    topics = db.relationship('Topic', backref='curriculum', lazy='dynamic')
+
+class Topic(db.Model):
+
+    __tablename__ = 'topics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    curriculum_id = db.Column(db.Integer, db.ForeignKey("curriculums.id"))
 
